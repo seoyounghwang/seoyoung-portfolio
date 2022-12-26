c529c64d35ba3e41062e8c99cf2d2095a470f2fc
SeoyoungHwang <dev.seoyoung.hwang@gmail.com>
2022-12-26 15:12:18 -0700

    Add portfolio

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HomeInventoryImg from "../assets/homeInventoryApp.JPG";
 import ManageEmployeeImg from "../assets/manageEmployee.JPG";
+import SamuraiPizzaImg from "../assets/samuraiPizza.JPG";
 
 const Work = () => {
   return (
@@ -58,6 +59,33 @@ const Work = () => {
                 <div className="pt-8 text-center">
                   <a
                     href="https://github.com/seoyounghwang/home-inventory-app"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${SamuraiPizzaImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center itmes-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div className="justify-center items-center flex-col pt-7 text-center">
+                <span className="text-2xl font-bold text-white tracking-wider justify-center items-center">
+                  MERN TypeScript GraphQL
+                </span>
+                <p className="text-1xl font-bold text-white tracking-wider justify-center">
+                  Samurai Pizza Application
+                </p>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://github.com/seoyounghwang/Samurai-Pizza-Cats-seoyounghwang"
                     target="_blank"
                     rel="noreferrer"
                   >
